4ed8fb6a3cd9801bc27287e15333c2fb6b492d5b
kojima <hide.k0712@gmail.com>
2025-09-18 21:39:23 +0900

    add the flask file server

---
+++ flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, send_file
+from flask_cors import cross_origin
+
+app = Flask(__name__)
+
+
+@app.route("/upload/<string:user>", methods=["POST"])
+@cross_origin(methods=["POST"])
+def upload_file(user):
+    code = request.form["code"]
+    with open(f"{user}_onshaked_handler.ino", "w") as f:
+        f.write(code)
+    return {"status": 200, "text": "OK"}
+
+
+@app.route("/download/<string:user>", methods=["GET"])
+def download_code(user):
+
+    downloadFileName = "onshaked_handler.ino"
+    downloadFile = f"{user}_onshaked_handler.ino"
+
+    return send_file(
+        downloadFile,
+        as_attachment=True,
+        attachment_filename=downloadFileName,
+        mimetype="text/plain",
+    )
